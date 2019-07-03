7ac026d7308c73f9aa6874f06ea115ce4c690136
Keith Isham <kisham8899@gmail.com>
2019-07-03 17:28:39 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -20,6 +20,11 @@ class Pokemon
     return new_pokemon
   end
   
+  def alter_hp(num, db)
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", [num], [self.id])
+    self.hp = num
+  end
+end
   
   
   
